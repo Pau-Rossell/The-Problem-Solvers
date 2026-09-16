ff8502a62bdee8562a9eab8a88508b64ab628176
Eric Ristol <eric@MacBook-Air.local>
2026-09-16 23:30:08 +0200

    DFS y BFS, solo cambian el metodo .pop()

--- 1.Blind_Search/algorithm.py
+++ 1.Blind_Search/algorithm.py
@@ -1,25 +1,55 @@
 import random
 
-"""Return (path, expanded, exploration): path includes start and goal; expanded counts visited nodes.
+"""
+Return (path, expanded, exploration): path includes start and goal; expanded counts visited nodes.
 
 grid: NumPy array, grid[row, column], 0 = free, 1 = obstacle.
 start, goal: (row, column). Only up/down/left/right moves are allowed.
 Return ([], expanded, exploration) when no path exists.
-
-Replace the random search demo with BFS and DFS and compare.
 """
+# DFS(Use .pop(-1))
+def solve(grid, start, goal):
+    # random search demo starts
+    frontier = [start]
+    parent = {start: None}
+    expanded = 0
+    exploration = []
+
+    while frontier:
+        
+        current = frontier.pop(-1) # LIFO
+        
+        expanded += 1
+        exploration.append(current)
+
+        if current == goal:
+            path = []
+            while current is not None:
+                path.append(current)
+                current = parent[current]
+            return path[::-1], expanded, exploration
+
+        r, c = current
+        for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            neighbor = (r + dr, c + dc)
+            nr, nc = neighbor
+            if 0 <= nr < grid.shape[0] and 0 <= nc < grid.shape[1] and grid[neighbor] == 0 and neighbor not in parent:
+                parent[neighbor] = current
+                frontier.append(neighbor)
+
+    # random search demo ends
+    return [], expanded, exploration
+
+# BFS(He eliminado el for loop porque no era necesario)(Use .pop(0))
 # def solve(grid, start, goal):
-#     # random search demo starts
 #     frontier = [start]
 #     parent = {start: None}
 #     expanded = 0
 #     exploration = []
 
 #     while frontier:
-#         # Random Search: randomly select and remove a node from the frontier
-#         idx = random.randrange(len(frontier))
-#         idx = 
-#         current = frontier.pop(idx)
+        
+#         current = frontier.pop(0) # FIFO
         
 #         expanded += 1
 #         exploration.append(current)
@@ -41,41 +71,3 @@ Replace the random search demo with BFS and DFS and compare.
 
 #     # random search demo ends
 #     return [], expanded, exploration
-
-
-
-#Algoritme per soliconar amb BFS
-def solve(grid, start, goal):
-    # random search demo starts
-    frontier = [start]
-    parent = {start: None}
-    expanded = 0
-    exploration = []
-
-    while frontier:
-        next_frontier = []
-        for _ in range(0, len(frontier)):
-            # BFS: iteratively go over all nodes in the frontier before updating it
-            current = frontier.pop(0)
-            
-            expanded += 1
-            exploration.append(current)
-
-            if current == goal:
-                path = []
-                while current is not None:
-                    path.append(current)
-                    current = parent[current]
-                return path[::-1], expanded, exploration
-
-            r, c = current
-            for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                neighbor = (r + dr, c + dc)
-                nr, nc = neighbor
-                if 0 <= nr < grid.shape[0] and 0 <= nc < grid.shape[1] and grid[neighbor] == 0 and neighbor not in parent:
-                    parent[neighbor] = current
-                    next_frontier.append(neighbor)
-        frontier = next_frontier
-
-    # random search demo ends
-    return [], expanded, exploration
